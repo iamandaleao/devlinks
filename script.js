1e97ad07dd1308e19412924e4e1baddd8910c8e1
Amanda Leão <amandabibiano144@gmail.com>
2025-04-07 10:21:51 -0300

    fix logo name

--- script.js
+++ script.js
@@ -8,9 +8,9 @@ console.log(img)
   // substituir a imagem
   if (html.classList.contains('light')) {
   // se tiver light mode, adicionar a imagem light
- img.setAttribute("src", "./assets/logo.jpg")
+ img.setAttribute("src", "./assets/logo-light.jpg")
   } else {
     // se tiver sem light mode, manter a imagem normal
-  img.setAttribute("src", "./assets/favicon.jpg")
+  img.setAttribute("src", "./assets/logo-dark.jpg")
   }
 }
